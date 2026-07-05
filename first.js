c96cd15094eef8a1741ce60ecdd8322b27b1d8c7
bhavani-mhrl <mchrlbhavani@gmail.com>
2026-07-05 17:13:18 +0530

    debugged search city

--- first.js
+++ first.js
@@ -1,24 +1,31 @@
+const searchInput = document.querySelector("#search-input");
+
 function changeDetails(response){
     let weatherImage=document.querySelector("#image-icon");
     let temp=document.querySelector("#weather-temperature");
     let cityName=document.querySelector("#weather-city");
     let humidity=document.querySelector("#humidity-temp");
     let windSpeed=document.querySelector("#windspeed-temp");
-    weatherImage.src=response.condition.icon_url;
-    temp.innerHTML=`${Math.round(response.temperature.current)}°C`;
-    cityName.innerHTML=response.city;
-    humidity.innerHTML=`${response.temperature.humidity}%`;
-    windSpeed.innerHTML=`${response.wind.speed}km/h`;
-}
-function searchCity(response){
-    console.log(response.data);
-    changeDetails(response.data);
+    weatherImage.src=response.data.condition.icon_url;
+    temp.innerHTML=`${Math.round(response.data.temperature.current)}°C`;
+    cityName.innerHTML=response.data.city;
+    humidity.innerHTML=`${response.data.temperature.humidity}%`;
+    windSpeed.innerHTML=`${response.data.wind.speed}km/h`;
 }
 
 function getWeatherApi(searchCityName){
-    let apiKey="fa802d0et31047o097e3a46943abb4fe";
-    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${searchCityName}&key=${apiKey}`;
-    axios.get(apiUrl).then(searchCity);
-    
+    if(searchCityName===""){
+        alert("Enter any city");
+    }else{
+        let apiKey="fa802d0et31047o097e3a46943abb4fe";
+        let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${searchCityName}&key=${apiKey}`;
+        axios.get(apiUrl).then(changeDetails);
+    }
 }
 getWeatherApi("paris");
+
+
+const submitButton = document.querySelector("#submit-button");
+submitButton.addEventListener("click",()=>{
+    getWeatherApi(searchInput.value);
+});
